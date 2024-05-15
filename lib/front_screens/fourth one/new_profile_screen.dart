6e13ae49e28d2efd42a5de113c00e69a2370f48a
romanapk <romanatahir420@gmail.com>
2024-05-15 11:40:02 +0500

    Update new_profile_screen.dart

--- lib/front_screens/fourth one/new_profile_screen.dart
+++ lib/front_screens/fourth one/new_profile_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart'; // for date formatting
 
 class UserProfileScreen extends StatelessWidget {
   // User data (replace with your data source)
-  final String name = "John Doe";
-  final String email = "johndoe@example.com";
-  final String phone = "(555) 555-5555";
+  final String name = "Romana Tahir";
+  final String email = "r@gmail.com";
+  final String phone = "+922858445";
   final DateTime birthday =
       DateTime(1990, 1, 1); // Replace with user's birthday
 
@@ -25,12 +24,12 @@ class UserProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'User Profile',
-          style: TextStyle(color: Colors.white), // Set app bar text color
-        ),
-        backgroundColor: Colors.teal, // Set a custom app bar color
-      ),
+          // title: Text(
+          //   'User Profile',
+          //   style: TextStyle(color: Colors.white), // Set app bar text color
+          // ),
+          // backgroundColor: Colors.teal, // Set a custom app bar color
+          ),
       body: SingleChildScrollView(
         padding: EdgeInsets.all(20.0),
         child: Column(
@@ -42,8 +41,8 @@ class UserProfileScreen extends StatelessWidget {
                 children: [
                   CircleAvatar(
                     radius: 50.0,
-                    backgroundImage: NetworkImage(
-                        'https://placeholdit.img/200x200'), // Replace with user's profile picture URL (optional)
+                    backgroundImage: AssetImage(
+                        'assets/user_icon.png'), // Replace with your asset path
                   ),
                   Positioned(
                     // Add a white border around the avatar
@@ -66,14 +65,14 @@ class UserProfileScreen extends StatelessWidget {
                 color: Colors.teal, // Set a custom color for name
               ),
             ),
-            SizedBox(height: 5.0),
-            Text(
-              email,
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.grey[600],
-              ),
-            ),
+            // SizedBox(height: 5.0),
+            // Text(
+            //   email,
+            //   style: TextStyle(
+            //     fontSize: 16.0,
+            //     color: Colors.grey[600],
+            //   ),
+            // ),
             SizedBox(height: 20.0),
             Divider(
                 thickness: 1.0, color: Colors.grey[300]), // Style the divider
@@ -82,12 +81,19 @@ class UserProfileScreen extends StatelessWidget {
               leading: Icon(Icons.phone, color: Colors.teal), // Set icon color
               title: Text(phone),
             ),
-            Divider(thickness: 1.0, color: Colors.grey[300]),
+            Divider(
+                thickness: 1.0, color: Colors.grey[300]), // Style the divider
+            SizedBox(height: 20.0),
             ListTile(
-              leading: Icon(Icons.cake, color: Colors.teal), // Set icon color
-              title: Text(DateFormat('y MMMM d')
-                  .format(birthday)), // Formatted birthday
+              leading: Icon(Icons.mail, color: Colors.teal), // Set icon color
+              title: Text(email),
             ),
+            // Divider(thickness: 1.0, color: Colors.grey[300]),
+            // ListTile(
+            //   leading: Icon(Icons.cake, color: Colors.teal), // Set icon color
+            //   title: Text(DateFormat('y MMMM d')
+            //       .format(birthday)), // Formatted birthday
+            // ),
             SizedBox(height: 20.0),
             _editProfileButton(context), // Add edit button if needed
           ],
