00cea7bd00682f038e46bbaec8dbfd304abfd207
romanapk <romanatahir420@gmail.com>
2024-07-08 09:36:40 +0500

    almost done

--- lib/customer_dashboard/search/view/search_view.dart
+++ lib/customer_dashboard/search/view/search_view.dart
@@ -5,22 +5,30 @@ import '../../doctor_profile/view/doctor_view.dart';
 
 class SearchView extends StatelessWidget {
   final String searchQuery;
-  const SearchView({super.key, required this.searchQuery});
+  const SearchView({Key? key, required this.searchQuery}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Styles.textColor,
-        title: "Search results".text.make(),
+        title: Text("Search results"),
         elevation: 0,
       ),
       body: FutureBuilder<QuerySnapshot>(
-        future: FirebaseFirestore.instance.collection('stylists').get(),
+        future: FirebaseFirestore.instance.collection('acceptedStylists').get(),
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
               child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text("No Stylist found, try another name"),
             );
           } else {
             var filteredStylists = snapshot.data!.docs.where((doc) {
@@ -32,7 +40,7 @@ class SearchView extends StatelessWidget {
 
             if (filteredStylists.isEmpty) {
               return Center(
-                child: "No Stylist found, try another name".text.makeCentered(),
+                child: Text("No Stylist found, try another name"),
               );
             }
 
@@ -41,13 +49,15 @@ class SearchView extends StatelessWidget {
               child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
-                  mainAxisExtent: 200,
+                  mainAxisExtent: 250,
                   crossAxisSpacing: 8,
                   mainAxisSpacing: 8,
                 ),
                 itemCount: filteredStylists.length,
                 itemBuilder: (BuildContext context, index) {
                   var doc = filteredStylists[index];
+                  String profilePictureUrl = doc['profilePicture'] ?? '';
+
                   return GestureDetector(
                     onTap: () {
                       Get.to(
@@ -64,28 +74,33 @@ class SearchView extends StatelessWidget {
                       ),
                       padding: const EdgeInsets.only(bottom: 5),
                       margin: const EdgeInsets.only(right: 8),
-                      height: 120,
-                      width: 130,
+                      height: 250,
+                      width: 180,
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           ClipRRect(
                             borderRadius: BorderRadius.circular(15),
-                            child: Container(
-                              width: 130,
-                              color: Styles.textColor,
-                              child: Image.asset(
-                                AppAssets.imgLogin,
-                                width: double.infinity,
-                                fit: BoxFit.cover,
-                              ),
+                            child: Image.network(
+                              profilePictureUrl,
+                              width: double.infinity,
+                              height: 150,
+                              fit: BoxFit.cover,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Image.asset(
+                                  AppAssets.imgLogin,
+                                  width: double.infinity,
+                                  height: 150,
+                                  fit: BoxFit.cover,
+                                );
+                              },
                             ),
                           ),
                           const Divider(),
-                          doc['stylistName']
-                              .toString()
-                              .text
-                              .size(AppFontSize.size16)
-                              .make(),
+                          Text(
+                            doc['stylistName'].toString(),
+                            style: TextStyle(fontSize: AppFontSize.size16),
+                          ),
                           VxRating(
                             onRatingUpdate: (value) {},
                             maxRating: 5,
